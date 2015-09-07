bba047a77feac5d7cb509ffaf9da80cf0722b073
Lucas Rumney <lucasrumney94@gmail.com>
2015-09-07 13:45:44 -0400

    Added inventory Display to the HUD

---
+++ LaborJam/Assets/Scripts/Utility/displayInventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class displayInventory : MonoBehaviour {
+
+	GameObject player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.gameObject.GetComponent<Text>().text = "Inventory : \n";
+		if (player.GetComponent<playerInventory>().inventory.Count > 0)
+		{
+			foreach (item i in player.GetComponent<playerInventory>().inventory)
+			{
+				this.gameObject.GetComponent<Text>().text += "\n" + i.name; 
+			}
+		}
+	}
+}
