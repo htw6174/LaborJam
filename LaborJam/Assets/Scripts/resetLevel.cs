30b6d0b9b0773396c872ad6eb76258d2d33a7398
Lucas Rumney <lucasrumney94@gmail.com>
2015-09-07 01:23:01 -0400

    Created Reset and Win Scripts;

---
+++ LaborJam/Assets/Scripts/resetLevel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resetLevel : MonoBehaviour {
+
+	public bool gameOver = false;
+	
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (gameOver)
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
