02311c79ea2a06833fad5a611e0531dcacec0ae6
Lucas Rumney <lucasrumney94@gmail.com>
2015-09-07 14:45:03 -0400

    working menus and volume sliders

---
+++ LaborJam/Assets/Scripts/sliderToMixerLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class sliderToMixerLevel : MonoBehaviour {
+
+	public string property;
+	public AudioMixer masterMixer;
+
+	private float VolumeSliderGet;
+	private float setValue;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		VolumeSliderGet = this.gameObject.GetComponent<Slider>().value;
+		setValue = (VolumeSliderGet-1)*80;
+		masterMixer.SetFloat(property, setValue );
+	}
+}
